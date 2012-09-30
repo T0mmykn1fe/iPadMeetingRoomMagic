583ddd1f5ba9f20c77e7f326430e38818629f1d7
Adam Ahmed <aahmed@atlassian.com>
2012-09-30 18:20:43 +1000

    Configured log rollover via winston. Added an etc folder for experimenting with GApps API. Added optional date param to getUpcomingEvents

---
+++ etc/googleRequest.js
@@ -0,0 +1,24 @@
+//for playing around with the Google Calendar API and seeing what data is returned.
+
+var gcal = require('../lib/gapps/gcal');
+var tokenHandler = require('../lib/gapps/tokenHandler');
+
+var clientId = '23835704985.apps.googleusercontent.com';
+var clientSecret = 'Pj1DAnZGNn8mfHMllmyrcKex';
+
+tokenHandler.getTokenData(clientId, clientSecret, function(err, tokenData) {
+	var calApi = gcal(tokenData, require('winston'));
+/*
+	calApi.getRooms(function(err, res) {
+		console.log(res);
+	});*/
+
+	var zoolander = 'atlassian.com_2d31363935383530352d343335@resource.calendar.google.com';
+	calApi.getUpcomingEvents(zoolander,
+		new Date(2012, 8, 26),
+		function(err, res) {
+		res.items.forEach(function(item) {
+			console.dir(item);
+		});
+	});
+});
