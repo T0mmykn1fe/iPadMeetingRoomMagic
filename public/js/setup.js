a051a07818c1a0936f1a8ef903870ec6793bb2fa
Adam Ahmed <aahmed@atlassian.com>
2014-10-05 21:19:06 +1100

    Cleanup
    - remove the resize from setup
    - rename setup_ui.js to setup.js
    - rename list.jade to setup.jade
    - remove vertical-align:middles. THen seem to make it less middle-aligned.
    - remove space before " No summary"

---
+++ public/js/setup.js
@@ -0,0 +1,14 @@
+    // By setting the font-size to 1/100th of the body height,
+    // we can use rem as a ghetto-vh. So 100rem means 100% of the body height
+    // and we can write all our sizes in terms of the viewport height.
+    function setFontSize() {
+        $(document.documentElement)
+            .css('font-size', $(document.body).height() / 100);
+    }
+
+    $(window).resize(setFontSize);
+    setFontSize();
+
+    $('li.no-status').click(function() {
+        window.location = $(this).attr('data-link');
+    });
